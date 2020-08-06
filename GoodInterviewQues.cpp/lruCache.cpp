044aa43d0a0f8c4cd6b241110ff94a317e5b267a
Madhav Nagpal <mr.madhavnagpal@gmail.com>
2020-08-06 10:06:09 +0530

    updated lru implementation
    Signed-off-by: Madhav Nagpal <mr.madhavnagpal@gmail.com>

--- GoodInterviewQues.cpp/lruCache.cpp
+++ GoodInterviewQues.cpp/lruCache.cpp
@@ -2,54 +2,76 @@
 // Krishna is Everything ...
 // Jai Shree Krishna
 
+//Least Recent Used Cache Implementation
+
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
-#define pb push_back
 const int N = 1e5 + 5, mod = 1e9 + 7;
 
-class lru
-{
-  list<int> dq;
-  map<int, list<int>::iterator> h;
-  int csize;
-
+class least_recent_used_cache {
 public:
-  lru(int n)
-  {
-    csize = n;
-  }
-  void refer(int x)
-  {
+    list<int> cache;
+    map<int, list<int>::iterator> h;
+    int max_size, curr_size;
 
-    if (h[x] == h.end())
-    {
+    least_recent_used_cache(int n) {
+        max_size = n;
+        curr_size = 0;
+    }
 
-      if (dq.size() == csize)
-      {
-        int last = dq.back();
-        h.erase(last);
-        dq.pop_back();
-      }
+    void insert_in_cache(int n) {
+        //whether number is already in cache or not
+        if (h.find(n) == h.end()) {
+            if (curr_size == max_size) {
+                int last = cache.back();
+                h.erase(last);
+                cache.pop_back();
+                curr_size--;
+            }
+        }
+        else {
+            cache.erase(h[n]);
+        }
+        cache.push_front(n);
+        h[n] = cache.begin();
+        curr_size++;
     }
-    else
-    {
-      dq.erase(h[x]);
+
+    void print() {
+        cout << "cache is:";
+        for (auto x : cache) {
+            cout << x << " ";
+        }
+        cout << endl;
     }
-    dq.push_front(x);
-    h[x] = dq.begin();
-  }
 };
 
 int32_t main()
 {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-#ifndef ONLINE_JUDGE
-  freopen("input.txt", "r", stdin);
-  freopen("output.txt", "w", stdout);
-#endif
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
 
-  return 0;
+    least_recent_used_cache cache1(4);
+    cache1.insert_in_cache(1);
+    cache1.insert_in_cache(2);
+    cache1.insert_in_cache(3);
+    cache1.insert_in_cache(4);
+    cache1.print();
+
+    cache1.insert_in_cache(2);
+    cache1.insert_in_cache(2);
+
+    cache1.print();
+
+    cache1.insert_in_cache(3);
+
+    cache1.print();
+
+    return 0;
 }
